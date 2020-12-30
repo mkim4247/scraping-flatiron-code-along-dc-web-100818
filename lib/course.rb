74b7c14ea67ad88ca3b99134d6397716c31f0d28
Michael Kim <m.kim4247@gmail.com>
2020-12-30 04:24:00 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,18 +1,18 @@
 class Course 
   attr_accessor :title, :schedule, :description
-  
+ 
   @@all = []
   
-  def initialize
-    self.class.all << self 
+  def initialize 
+    @@all << self 
   end 
   
-  def self.all
-    @@all 
+  def self.all 
+    @@all
   end 
   
-  def self.reset_all
-    self.all.clear
+  def self.reset_all 
+    @@all.clear 
   end 
   
 end 
